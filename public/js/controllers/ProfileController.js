773c1d78c91df13199b19c0d29e5cecf85f61dca
gmdiniz <gabrieldinizmota@hotmail.com>
2021-01-24 21:20:26 -0300

    Padronizando modal

--- public/js/controllers/ProfileController.js
+++ public/js/controllers/ProfileController.js
@@ -28,29 +28,31 @@ meca_app.controller('ProfileController', function ($scope, UserModel, $location)
 
     $scope.user = UserModel.getUserData();
     
-    var modal = document.getElementById("myModal");
+    var veiculoModal = document.getElementById("veiculoModal");
+    var chamadoModal = document.getElementById("chamadoModal");
 
     // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+    var veiculoBtn = document.getElementById("veiculoBtn");
+    var chamadoBtn = document.getElementById("chamadoBtn");
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    var spanVeiculo = document.getElementById("closeVeiculo");
+    var spanChamado = document.getElementById("closeChamado");
 
     // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-    modal.style.display = "block";
+    veiculoBtn.onclick = function() {
+        veiculoModal.style.display = "block";
+    }
+    chamadoBtn.onclick = function() {
+        chamadoModal.style.display = "block";
     }
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-    modal.style.display = "none";
+    spanVeiculo.onclick = function() {
+        veiculoModal.style.display = "none";
     }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
+    spanChamado.onclick = function() {
+        chamadoModal.style.display = "none";
     }
 
     $scope.home = function () {
